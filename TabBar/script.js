728ae9d07c09ba42a5460fafcc96197afe4eb100
vajihemansoori <vajihe.mansoori76@gmail.com>
2022-06-21 23:34:44 +0430

    add file tabbar

---
+++ TabBar/script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll("nav ul li i");
+const light = document.querySelector("nav .tubelight");
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    items.forEach((i) => {
+      i.classList.remove("active");
+      i.classList.remove("pd");
+    });
+    item.classList.add("active");
+    item.classList.toggle("pd");
+    light.style.left = `${item.offsetLeft + light.offsetWidth / 4}px`;
+  });
+});
